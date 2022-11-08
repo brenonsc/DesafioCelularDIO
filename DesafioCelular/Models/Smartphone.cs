cf232a000e46da15590940718275b026bcaeeb1a
Breno Henrique Nascimento <brenonsc@gmail.com>
2022-11-08 20:10:53 -0300

    Desafio completo

---
+++ DesafioCelular/Models/Smartphone.cs
@@ -0,0 +1,30 @@
+namespace DesafioCelular.Models;
+
+public abstract class Smartphone
+{
+    public string Numero { get; set; }
+    public string Modelo { get; set; }
+    public string IMEI { get; set; }
+    public int Memoria { get; set; }
+
+    public Smartphone(string numero, string modelo, string imei, int memoria)
+    {
+        // TODO: Passar os parâmetros do construtor para as propriedades
+        this.Numero = numero;
+        this.Modelo = modelo;
+        this.IMEI = imei;
+        this.Memoria = memoria;
+    }
+
+    public void Ligar()
+    {
+        Console.WriteLine("Ligando...");
+    }
+
+    public void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação...");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
+}
---
+++ DesafioCelular/Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioCelular.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("(11) 99999-9999", "Nokia", "imeiNokia", 4);
+Iphone iphone = new Iphone("(11) 99999-9999", "Iphone", "imeiIphone", 8);
+
+//Testes Nokia
+Console.WriteLine("TESTES NOKIA");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("NMaps");
+
+//Testes Iphone
+Console.WriteLine("\n\nTESTES IPHONE");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("iCloud");
